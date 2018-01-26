fcbbd3f6c77b5e1e6ce270c8b4a9c2fb95369115
Josh <josharmstrongphoto@gmail.com>
2018-01-26 10:59:03 -0800

    Initial commit

---
+++ epicspies/epicspies/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="epicspies.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>
+            <asp:Image ID="Image1" runat="server" Height="190px" ImageUrl="http://learnvisualstudio.kenjlucas.com/WebApps/EpicSpiesChallenge02/epic-spies-logo.jpg" Width="140px" />
+        </h1>
+        <h1>Spy New Assignment Form</h1>
+        <p>
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="codeNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="assignmentNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            End Date of Previous Assignment<asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+        </p>
+        <p>
+            Start Date of New Assignment<asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            Projected End Date of&nbsp; New Assignment<asp:Calendar ID="Calendar3" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            <asp:Button ID="assignSpyButton" runat="server" OnClick="assignSpyButton_Click" Text="Assign Spy" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ epicspies/epicspies/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace epicspies
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Calendar1.SelectedDate = DateTime.Now.Date;
+                Calendar2.SelectedDate = DateTime.Now.Date.AddDays(14);
+                Calendar3.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assignSpyButton_Click(object sender, EventArgs e)
+        {
+            TimeSpan totalDurationOfAssignment = Calendar3.SelectedDate.Subtract(Calendar2.SelectedDate);
+            double totalCost = totalDurationOfAssignment.TotalDays * 500.0;
+            if (totalDurationOfAssignment.TotalDays > 21)
+            {
+                totalCost += 1000.0;
+            }
+            resultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}", codeNameTextBox.Text, assignmentNameTextBox.Text, totalCost);
+            TimeSpan timeBetweenAssignments = Calendar2.SelectedDate.Subtract(Calendar1.SelectedDate);
+            if (timeBetweenAssignments.TotalDays < 14)
+            {
+                resultLabel.Text = "Error: Must allow at least 2 weeks between assignments.";
+
+                DateTime earliestNewAssignmentDate = Calendar1.SelectedDate.AddDays(14);
+                Calendar2.SelectedDate = earliestNewAssignmentDate;
+                Calendar2.VisibleDate = earliestNewAssignmentDate;
+            }
+        }
+    }
+}
